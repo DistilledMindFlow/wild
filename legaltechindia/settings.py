318e292cbd7191e7fbdbb55120aec4ad75fabe63
Ansari Majid Ali <taznoboys@gmail.com>
2024-03-25 14:42:24 +0530

    Update settings.py

--- legaltechindia/settings.py
+++ legaltechindia/settings.py
@@ -144,6 +144,6 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
